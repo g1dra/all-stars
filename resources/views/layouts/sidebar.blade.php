94d12e57e7b1e815c2346a5e3a738c67ebf71dde
Darko Vucetic <darko@kod.io>
2018-11-06 10:46:13 +0100

    frontend

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -13,7 +13,7 @@
                 <section class="match-preview__body">
                     <header class="match-preview__header">
                         <h3 class="match-preview__title">Prva B crnogorska liga</h3>
-                        <time class="match-preview__date" datetime="2017-05-17">17/11/2018</time>
+                        <time class="match-preview__date" datetime="06-11-2018">06-11-2018</time>
                     </header>
                     <div class="match-preview__content">
 
@@ -40,7 +40,7 @@
                             <figure class="match-preview__team-logo">
                                 <img src="assets/images/clubs/rudar150x150.png" alt="">
                             </figure>
-                            <h5 class="match-preview__team-name">Clovers</h5>
+                            <h5 class="match-preview__team-name">Rudar</h5>
                             {{--<div class="match-preview__team-info">ST Paddy's Institute</div>--}}
                         </div>
                         <!-- 2nd Team / End -->
@@ -74,10 +74,10 @@
                 <table class="table table-hover table-standings">
                     <thead>
                     <tr>
-                        <th>Team Positions</th>
+                        <th>Pos.</th>
                         <th>W</th>
                         <th>L</th>
-                        <th>GB</th>
+                        <th>P</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -86,131 +86,98 @@
                         <td>
                             <div class="team-meta">
                                 <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/pirates_shield.png" alt="L.A Pirates">
+                                    <img src="assets/images/clubs/all-stars150x150.png" alt="All Stars">
                                 </figure>
                                 <div class="team-meta__info">
-                                    <h6 class="team-meta__name">L.A Pirates</h6>
-                                    <span class="team-meta__place">Bebop Institute</span>
+                                    <h6 class="team-meta__name">All Stars</h6>
+                                    {{--<span class="team-meta__place">Bebop Institute</span>--}}
                                 </div>
                             </div>
                         </td>
-                        <td>45</td>
-                        <td>5</td>
+                        <td>2</td>
                         <td>0</td>
+                        <td>4</td>
                     </tr>
                     <tr>
                         <td>
                             <div class="team-meta">
                                 <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/sharks_shield.png" alt="Sharks">
+                                    <img src="assets/images/clubs/teodo150x150.png" alt="Teodo">
                                 </figure>
                                 <div class="team-meta__info">
-                                    <h6 class="team-meta__name">Sharks</h6>
-                                    <span class="team-meta__place">Marine College</span>
+                                    <h6 class="team-meta__name">Teodo</h6>
+                                    {{--<span class="team-meta__place">Marine College</span>--}}
                                 </div>
                             </div>
                         </td>
-                        <td>42</td>
-                        <td>8</td>
+                        <td>2</td>
+                        <td>0</td>
+                        <td>4</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="team-meta">
+                                <figure class="team-meta__logo">
+                                    <img src="assets/images/clubs/rudar150x150.png" alt="Rudar">
+                                </figure>
+                                <div class="team-meta__info">
+                                    <h6 class="team-meta__name">Rudar</h6>
+                                    {{--<span class="team-meta__place">Eric Bros School</span>--}}
+                                </div>
+                            </div>
+                        </td>
+                        <td>1</td>
+                        <td>1</td>
                         <td>3</td>
                     </tr>
                     <tr>
                         <td>
                             <div class="team-meta">
                                 <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/alchemists_b_shield.png" alt="The Alchemists">
+                                    <img src="assets/images/clubs/kotor150x150.png" alt="Kotor">
                                 </figure>
                                 <div class="team-meta__info">
-                                    <h6 class="team-meta__name">The Alchemists</h6>
-                                    <span class="team-meta__place">Eric Bros School</span>
+                                    <h6 class="team-meta__name">Kotor</h6>
+                                    {{--<span class="team-meta__place">Bay College</span>--}}
                                 </div>
                             </div>
                         </td>
-                        <td>40</td>
-                        <td>10</td>
-                        <td>5</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>3</td>
                     </tr>
                     <tr>
                         <td>
                             <div class="team-meta">
                                 <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/ocean_kings_shield.png" alt="Ocean Kings">
+                                    <img src="assets/images/clubs/lim150x150.png" alt="Lim">
                                 </figure>
                                 <div class="team-meta__info">
-                                    <h6 class="team-meta__name">Ocean Kings</h6>
-                                    <span class="team-meta__place">Bay College</span>
+                                    <h6 class="team-meta__name">Lim</h6>
+                                    {{--<span class="team-meta__place">Icarus College</span>--}}
                                 </div>
                             </div>
                         </td>
-                        <td>38</td>
-                        <td>12</td>
-                        <td>7</td>
+                        <td>0</td>
+                        <td>0</td>
+                        <td>2</td>
                     </tr>
                     <tr>
                         <td>
                             <div class="team-meta">
                                 <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/red_wings_shield.png" alt="Red Wings">
+                                    <img src="assets/images/clubs/strsljen150x150.png" alt="Stršljen">
                                 </figure>
                                 <div class="team-meta__info">
-                                    <h6 class="team-meta__name">Red Wings</h6>
-                                    <span class="team-meta__place">Icarus College</span>
+                                    <h6 class="team-meta__name">Stršljen</h6>
+                                    {{--<span class="team-meta__place">St. Patrick’s Institute</span>--}}
                                 </div>
                             </div>
                         </td>
-                        <td>37</td>
-                        <td>13</td>
-                        <td>8</td>
+                        <td>0</td>
+                        <td>2</td>
+                        <td>2</td>
                     </tr>
-                    <tr>
-                        <td>
-                            <div class="team-meta">
-                                <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/lucky_clovers_shield.png" alt="Lucky Clovers">
-                                </figure>
-                                <div class="team-meta__info">
-                                    <h6 class="team-meta__name">Lucky Clovers</h6>
-                                    <span class="team-meta__place">St. Patrick’s Institute</span>
-                                </div>
-                            </div>
-                        </td>
-                        <td>34</td>
-                        <td>16</td>
-                        <td>11</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="team-meta">
-                                <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/draconians_shield.png" alt="Draconians">
-                                </figure>
-                                <div class="team-meta__info">
-                                    <h6 class="team-meta__name">Draconians</h6>
-                                    <span class="team-meta__place">High Rock College</span>
-                                </div>
-                            </div>
-                        </td>
-                        <td>31</td>
-                        <td>19</td>
-                        <td>14</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="team-meta">
-                                <figure class="team-meta__logo">
-                                    <img src="assets/images/samples/logos/bloody_wave_shield.png" alt="Bloody Wave">
-                                </figure>
-                                <div class="team-meta__info">
-                                    <h6 class="team-meta__name">Bloody Wave</h6>
-                                    <span class="team-meta__place">Atlantic School</span>
-                                </div>
-                            </div>
-                        </td>
-                        <td>30</td>
-                        <td>20</td>
-                        <td>15</td>
-                    </tr>
-
                     </tbody>
                 </table>
             </div>
@@ -220,7 +187,7 @@
 
     <!-- Widget: Social Buttons -->
     <aside class="widget widget--sidebar widget-social">
-        <a href="#" class="btn-social-counter btn-social-counter--fb" target="_blank">
+        <a href="https://www.facebook.com/Kosarkaski-klub-All-Stars-321895358175219/" class="btn-social-counter btn-social-counter--fb" target="_blank">
             <div class="btn-social-counter__icon">
                 <i class="fa fa-facebook"></i>
             </div>
@@ -228,15 +195,15 @@
             <span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Likes</span>
             <span class="btn-social-counter__add-icon"></span>
         </a>
-        <a href="#" class="btn-social-counter btn-social-counter--twitter" target="_blank">
+        <a href="https://www.instagram.com/k.k.allstars02/?hl=en" class="btn-social-counter btn-social-counter--instagram" target="_blank">
             <div class="btn-social-counter__icon">
-                <i class="fa fa-twitter"></i>
+                <i class="fa fa-instagram"></i>
             </div>
-            <h6 class="btn-social-counter__title">Follow Us on Twitter</h6>
+            <h6 class="btn-social-counter__title">Follow Us on Instagram</h6>
             <span class="btn-social-counter__count"><span class="btn-social-counter__count-num"></span> Followers</span>
             <span class="btn-social-counter__add-icon"></span>
         </a>
-        <a href="#" class="btn-social-counter btn-social-counter--rss" target="_blank">
+       {{-- <a href="#" class="btn-social-counter btn-social-counter--rss" target="_blank">
             <div class="btn-social-counter__icon">
                 <i class="fa fa-rss"></i>
             </div>
@@ -244,7 +211,7 @@
             <span class="btn-social-counter__count"><span
                         class="btn-social-counter__count-num">840</span> Subscribers</span>
             <span class="btn-social-counter__add-icon"></span>
-        </a>
+        </a>--}}
     </aside>
     <!-- Widget: Social Buttons / End -->
 
@@ -359,18 +326,18 @@
     <!-- Widget: Game Result -->
     <aside class="widget card widget--sidebar widget-game-result">
         <div class="widget__title card__header card__header--has-btn">
-            <h4>Last Game Results</h4>
-            <button class="btn btn-default btn-outline btn-xs card-header__button js-switch-toggle"><span
-                        class="js-switch-txt" data-text-expand="Expand Stats" data-text-shrink="Shrink Stats">Expand Stats</span>
-            </button>
+            <h4>Poslednji rezultati</h4>
+            {{--<button class="btn btn-default btn-outline btn-xs card-header__button js-switch-toggle"><span--}}
+                        {{--class="js-switch-txt" data-text-expand="Expand Stats" data-text-shrink="Shrink Stats">Expand Stats</span>--}}
+            {{--</button>--}}
         </div>
         <div class="widget__content card__content">
             <!-- Game Score -->
             <div class="widget-game-result__section">
                 <div class="widget-game-result__section-inner">
                     <header class="widget-game-result__header">
-                        <h3 class="widget-game-result__title">Championship Quarter Finals</h3>
-                        <time class="widget-game-result__date" datetime="2016-03-24">Saturday, March 24th, 2016</time>
+                        <h3 class="widget-game-result__title">Drugo kolo</h3>
+                        <time class="widget-game-result__date" datetime="2016-03-24">4/11/2018</time>
                     </header>
 
                     <div class="widget-game-result__main">
@@ -378,34 +345,34 @@
                         <div class="widget-game-result__team widget-game-result__team--first">
                             <figure class="widget-game-result__team-logo">
                                 <a href="#"><img
-                                            src="assets/images/samples/logos/alchemists_last_game_results_small.png"
+                                            src="assets/images/clubs/all-stars150x150.png"
                                             alt=""></a>
                             </figure>
                             <div class="widget-game-result__team-info">
-                                <h5 class="widget-game-result__team-name">Alchemists</h5>
-                                <div class="widget-game-result__team-desc">Elric Bros School</div>
+                                <h5 class="widget-game-result__team-name">All stars</h5>
+                                {{--<div class="widget-game-result__team-desc">Elric Bros School</div>--}}
                             </div>
                         </div>
                         <!-- 1st Team / End -->
 
                         <div class="widget-game-result__score-wrap">
                             <div class="widget-game-result__score">
-                                <span class="widget-game-result__score-result widget-game-result__score-result--winner">107</span>
+                                <span class="widget-game-result__score-result widget-game-result__score-result--winner">67</span>
                                 <span class="widget-game-result__score-dash">-</span> <span
-                                        class="widget-game-result__score-result widget-game-result__score-result--loser">102</span>
+                                        class="widget-game-result__score-result widget-game-result__score-result--loser">46</span>
                             </div>
-                            <div class="widget-game-result__score-label">Final Score</div>
+                            <div class="widget-game-result__score-label">Rezultat</div>
                         </div>
 
                         <!-- 2nd Team -->
                         <div class="widget-game-result__team widget-game-result__team--second">
                             <figure class="widget-game-result__team-logo">
-                                <a href="#"><img src="assets/images/samples/logos/sharks_last_game_results_small.png"
+                                <a href="#"><img src="assets/images/clubs/strsljen150x150.png"
                                                  alt=""></a>
                             </figure>
                             <div class="widget-game-result__team-info">
-                                <h5 class="widget-game-result__team-name">Sharks</h5>
-                                <div class="widget-game-result__team-desc">Marine College</div>
+                                <h5 class="widget-game-result__team-name">Stršljen</h5>
+                                {{--<div class="widget-game-result__team-desc">Marine College</div>--}}
                             </div>
                         </div>
                         <!-- 2nd Team / End -->
