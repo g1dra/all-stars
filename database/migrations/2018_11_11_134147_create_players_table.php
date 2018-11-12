25ba55b2fbbc684738fd428bc69468584844ce6f
Darko Vucetic <darko.vucetic7@gmail.com>
2018-11-12 02:13:52 +0100

    svasta nesto

---
+++ database/migrations/2018_11_11_134147_create_players_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('date_of_birth')->nullable();
+            $table->integer('height')->nullable();
+            $table->string('position')->nullable();
+            $table->integer('number')->nullable();
+            $table->string('img')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
